cc9106e23be50aca778bbfc57af4afe68dddea7d
Wonkydan <wonkydan@gmail.com>
2020-12-14 18:56:27 +0000

    Completed the detail page generically.

--- src/app/home/receipt-detail/receipt-detail.page.ts
+++ src/app/home/receipt-detail/receipt-detail.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Receipt } from 'src/app/models/receipt-model';
+import { ReceiptService } from '../../services/receipt-service.service';
 
 @Component({
-  selector: 'app-receipt-detail',
-  templateUrl: './receipt-detail.page.html',
-  styleUrls: ['./receipt-detail.page.scss'],
+	selector: 'app-receipt-detail',
+	templateUrl: './receipt-detail.page.html',
+	styleUrls: [ './receipt-detail.page.scss' ]
 })
 export class ReceiptDetailPage implements OnInit {
+	receipt: Receipt;
 
-  constructor() { }
+	constructor(
+		private receiptService: ReceiptService,
+		private navCtrl: NavController,
+		private activatedRoute: ActivatedRoute
+	) {}
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.activatedRoute.paramMap.subscribe((paramMap) => {
+			if (!paramMap.has('receiptId')) {
+				this.navCtrl.navigateBack('/home');
+				return;
+			}
+			const placeId = paramMap.get('receiptId');
+			console.log(placeId);
+			this.receipt = this.receiptService.getCurrentReceipt(placeId);
+		});
+	}
 }
